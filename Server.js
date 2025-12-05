c51c0b57d69d36127cc5678a96f30ce621a8abbe
MrManoj124 <manojmanorooban@gmail.com>
2025-12-05 11:02:31 +0530

    use global variable

--- Server.js
+++ Server.js
@@ -1,9 +1,9 @@
 console.log("Manorooban");
-//console.log(global);
+console.log(global);
 
-const OperatingSystem = require('os');
-console.log(OperatingSystem.type());
-console.log(OperatingSystem.version());
-console.log(OperatingSystem.homedir());
+// const OperatingSystem = require('os');
+// console.log(OperatingSystem.type());
+// console.log(OperatingSystem.version());
+// console.log(OperatingSystem.homedir());
 // console.log(__dirname);
 // console.log(__filename);
