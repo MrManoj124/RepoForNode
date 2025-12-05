2903ef3639e568cf4ed559bd47309798c152ddfa
MrManoj124 <manojmanorooban@gmail.com>
2025-12-05 11:20:14 +0530

    initialize one variable and get dirname,basename,extname

--- Server.js
+++ Server.js
@@ -1,5 +1,5 @@
 console.log("Manorooban");
-console.log(global);
+// console.log(global);
 
 const OperatingSystem = require('os');
 console.log(OperatingSystem.type());
@@ -7,3 +7,9 @@ console.log(OperatingSystem.version());
 console.log(OperatingSystem.homedir());
 console.log(__dirname);
 console.log(__filename);
+console.log(OperatingSystem.dirname(__dirname));
+console.log(OperatingSystem.basename(__filename));
+console.log(OperatingSystem.extname(__filename));
+console.log(OperatingSystem.parse(__filename));
+ 
+
