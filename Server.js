90ab9696dad55deaa3437d786e5f944bce11f5f3
MrManoj124 <manojmanorooban@gmail.com>
2025-12-05 11:29:21 +0530

    create Math.js file

--- Server.js
+++ Server.js
@@ -1,15 +1,16 @@
 console.log("Manorooban");
 // console.log(global);
 
-const OperatingSystem = require('os');
-console.log(OperatingSystem.type());
-console.log(OperatingSystem.version());
-console.log(OperatingSystem.homedir());
-console.log(__dirname);
-console.log(__filename);
-console.log(OperatingSystem.dirname(__dirname));
-console.log(OperatingSystem.basename(__filename));
-console.log(OperatingSystem.extname(__filename));
-console.log(OperatingSystem.parse(__filename));
+// const OperatingSystem = require('os');
+// console.log(OperatingSystem.type());
+// console.log(OperatingSystem.version());
+// console.log(OperatingSystem.homedir());
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(OperatingSystem.dirname(__dirname));
+// console.log(OperatingSystem.basename(__filename));
+// console.log(OperatingSystem.extname(__filename));
+// console.log(OperatingSystem.parse(__filename));
  
-
+//const Addition = require('./Math');
+//console.log(Addition.add(50,35));
