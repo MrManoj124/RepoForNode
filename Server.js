b52bd9f3ca3e7bb1473796879c05f71e70f32b10
MrManoj124 <manojmanorooban@gmail.com>
2025-12-06 10:49:59 +0530

    Use PORT to host a website localserver

--- Server.js
+++ Server.js
@@ -28,5 +28,19 @@ console.log(Addition.divide(50,35));
 // console.log(multiply(50, 35));
 // console.log(divide(50, 35));   
 
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+const PORT = 3000;
+const HOSTNAME = 'Manorooban';
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+});
+
 
 
