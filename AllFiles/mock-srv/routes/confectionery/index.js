2f792f0138c566aa7ca8dd73499afa950c1d17b8
MrManoj124 <manojmanorooban@gmail.com>
2026-01-14 00:04:48 +0530

    update confectionary/index.js file

--- AllFiles/mock-srv/routes/confectionery/index.js
+++ AllFiles/mock-srv/routes/confectionery/index.js
@@ -1,10 +1,15 @@
 'use strict'
 
-module.exports = async function (fastify, opts){
-    fastify.get('/', async function(request, reply){
-        return [{
-            id:'B1', name: 'Chocolate Bar', rrp:'22.40',
-            info: 'Delecious overpriced chocolate.'
-        }]
-    })
+const data = [
+  {id: 'B1', name: 'Chocolate Bar', rrp: '22.40', info: 'Delicious overpriced chocolate.'}
+]
+
+module.exports = async function (fastify, opts) {
+  fastify.get('/', async function (request, reply) {
+    return data
+  })
+  fastify.post('/', async function (request, reply) {
+    request.mockDataInsert(opts.prefix.slice(1), data)
+    return data
+  })
 }
