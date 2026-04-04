2f67b0fa7de10794723c330e28ae3d06208a4380
MrManoj124 <manojmanorooban@gmail.com>
2026-04-04 22:03:06 +0530

    create userSchema in User.js

--- LMS/backend/models/User.js
+++ LMS/backend/models/User.js
@@ -1,5 +1,22 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    
-})
+    name : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    role : {
+        type : String,
+        enum : ["Student","Instructor","Admin"],
+        default : "Student"
+    }
+}, {timestamps : true});
