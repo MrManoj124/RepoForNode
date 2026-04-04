14e8d4e597a927f40fcf0b333395a4302f9f6049
MrManoj124 <manojmanorooban@gmail.com>
2026-04-04 22:29:59 +0530

    Start to create a registerUser function

--- LMS/backend/controllers/authController.js
+++ LMS/backend/controllers/authController.js
@@ -2,3 +2,7 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const generateToken = require("../utils/generateToken");
 
+//@Desc Register user
+exports.registerUser = async (req, res) => {
+    
+}
